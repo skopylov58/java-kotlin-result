f8636e76e6d07e36133ab489a6a33b49a4726794
Sergey Kopylov <skopylov@gmail.com>
2023-03-05 16:55:32 +0300

    feat: added exception interceptor

--- src/main/java/result/Failure.java
+++ src/main/java/result/Failure.java
@@ -1,9 +1,31 @@
 package result;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public record Failure<T>(Exception exception) implements Result<T> {
 
+    private transient static final Consumer<Exception> interceptor;
+    
+    static {
+        String className = System.getProperty("result.interceptor");
+        if (className != null) {
+            try {
+                interceptor = (Consumer<Exception>) Class.forName(className).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            interceptor = null; 
+        }
+    }
+    
+    public Failure {
+        if (interceptor != null) {
+            interceptor.accept(exception);
+        }
+    }
+    
     static <T> Result<T> of(Exception e) {
         return new Failure<T>(e);
     }
